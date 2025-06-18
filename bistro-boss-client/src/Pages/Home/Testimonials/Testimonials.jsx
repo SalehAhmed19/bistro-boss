e447cfeef9a5a83adca0b491104d2a4707bd4da6
SalehAhmed19 <saleh.ahmed.mahin@gmail.com>
2025-06-18 15:42:38 +0600

    testimonial-added

---
+++ bistro-boss-client/src/Pages/Home/Testimonials/Testimonials.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import quote from "../../../assets/icon/quote.svg";
+
+import { Rating } from "@smastrom/react-rating";
+
+import "@smastrom/react-rating/style.css";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+// import required modules
+import { Navigation } from "swiper/modules";
+
+export default function Testimonials() {
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch("reviews.json")
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, []);
+
+  return (
+    <div className="my-10">
+      <SectionTitle title={"Testimonials"} subtitle={"What our client say"} />
+      <div>
+        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+          {reviews.map((review) => (
+            <SwiperSlide key={review._id}>
+              <div className="my-10 mx-24 text-center flex flex-col items-center gap-5">
+                <Rating
+                  style={{ maxWidth: 180 }}
+                  value={review.rating}
+                  readOnly
+                />
+                <img src={quote} alt="" />
+                <p>{review.details}</p>
+                <p className="text-orange-400 uppercase text-2xl">
+                  {review.name}
+                </p>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
+}
